db3d3d0ad7643d5d35b22562591e2c1aff25e95b
MD Naimur Rahman <69746759+MNR-Tushar@users.noreply.github.com>
2024-03-16 02:10:12 +0600

    Create Template 1.cpp

---
+++ Template 1.cpp
@@ -0,0 +1,35 @@
+//Please allah help me.
+#include<bits/stdc++.h>
+using namespace std;
+#define     ll              long long int
+#define endl '\n'
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+const int N=1e5+123;
+int mod=1e7+123;
+void solve()
+{
+    int n;
+    cin>>n;
+    
+
+    
+
+    
+
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    
+
+
+    return 0;
+}
