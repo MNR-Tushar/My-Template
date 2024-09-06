0a0bb8e68b8e4e9d5d113157f8e5878ea661ffbe
MD Naimur Rahman <69746759+MNR-Tushar@users.noreply.github.com>
2024-09-06 12:27:34 +0600

    Create Template.cpp

---
+++ VS code/Template.cpp
@@ -0,0 +1,35 @@
+//Please allah help me.
+#include<bits/stdc++.h>
+using namespace std;
+#define     ll              long long int
+#define endl '\n'
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+int t=1;
+const int N=1e5+123;
+int mod=1e7+123;
+void solve()
+{
+    int n;
+    cin>>n;
+
+  
+
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.in", "r", stdin);
+    freopen("output.in", "w",stdout);
+#endif   
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    
+
+
+    return 0;
+}
